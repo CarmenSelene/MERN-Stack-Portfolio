a4fa0cd4c56939d3c95e0366951fd8caec1c7e42
Saryn Caister <thegreenmajority@gmail.com>
2019-03-18 13:19:53 -0400

    updated giphy api page description

--- client/src/components/Giphysearch/SearchForm.js
+++ client/src/components/Giphysearch/SearchForm.js
@@ -3,22 +3,22 @@ import "./Search.css";
 
 function SearchForm(props) {
   return (
-      <form className="giphySeachInputBar">
-        <div className="formInput">
-          <input
-            onChange={props.handleInputChange}
-            value={props.search}
-            name="search"
-            type="text"
-            className="form-control"
-            placeholder="Search for a Gif"
-            id="search"
-          />
-          <button onClick={props.handleFormSubmit} className="btn btn-primary btn-lg mt-3">
-            Search
+    <div className="giphySearchBar">
+      <form className="formInput">
+        <input
+          onChange={props.handleInputChange}
+          value={props.search}
+          name="search"
+          type="text"
+          className="form-control gifSearchBar"
+          placeholder="Search for a Gif"
+          id="search"
+        />
+        <button onClick={props.handleFormSubmit} className="btn btn-primary" id="giphySearchButton">
+          Search
         </button>
-        </div>
       </form>
+    </div>
   );
 }
 
--- client/src/routes/Giphy/Giphytext.js
+++ client/src/routes/Giphy/Giphytext.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "./Giphy.css";
 
-function Abouttext() {
+function Giphytext() {
     return (
-        <div className="centeredContent">
-            <h1>This page uses Giphy API</h1>
+        <div className="wrapper">
+            <h1>Axios/API Demo</h1>
+            <p className="giphyText">This page uses a bootstrap form to accept "string" text input from the user and send it to the "Giphy" API via Axios. The below client side map function displays the first 10 results in neat boxes with custom styling.</p>
         </div>
     );
 }
 
-export default Abouttext;
+export default Giphytext;
